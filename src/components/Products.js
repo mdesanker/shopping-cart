2b082e66967ebfcf06927affd1bee76d26427d0a
Michael Desanker <mdesanker@gmail.com>
2021-11-19 06:30:57 -0500

    feat: add async fetch function to get api data

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,29 @@
 import React, { Fragment, useEffect } from "react";
 
 const Products = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://swapi.dev/api/vehicles/`, {
+        mode: "cors",
+      });
+
+      // Error in fetch
+      if (!response.ok) {
+        const msg = `An error has occured: ${response.status}`;
+        throw new Error(msg);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Fragment>
       <h1>Hello from Products</h1>
