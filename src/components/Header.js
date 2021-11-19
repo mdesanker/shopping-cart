dd23ca18236aaa21f901bb860636e566bb46dad3
Michael Desanker <mdesanker@gmail.com>
2021-11-18 19:19:46 -0500

    feat: add links to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = () => {
   return (
     <HeaderContainer>
       <Nav>
-        <h3>Shop'd</h3>
-        <ul>
-          <li>Products</li>
+        <NavLink to="/">
+          <h3>Shop'd</h3>
+        </NavLink>
+        <NavUnordered>
+          <NavLink to="/products">
+            <li>Products</li>
+          </NavLink>
           <li>Contact</li>
           <li>
             <i className="fas fa-shopping-cart"></i>
           </li>
-        </ul>
+        </NavUnordered>
       </Nav>
     </HeaderContainer>
   );
@@ -20,6 +25,23 @@ const Header = () => {
 const HeaderContainer = styled.header`
   display: flex;
   justify-content: center;
+`;
+
+const NavUnordered = styled.ul`
+  display: flex;
+  gap: 2vw;
+
+  & li {
+    color: black;
+    list-style: none;
+    padding: 20px;
+  }
+
+  & .current {
+    li {
+      border-bottom: 2px solid black;
+    }
+  }
 `;
 
 const Nav = styled.nav`
@@ -31,18 +53,9 @@ const Nav = styled.nav`
   max-width: 1200px;
 
   & h3 {
+    color: black;
     padding: 20px;
     letter-spacing: 0.1rem;
-  }
-
-  & ul {
-    display: flex;
-    gap: 2vw;
-  }
-
-  & ul li {
-    list-style: none;
-    padding: 20px;
   }
 `;
 
