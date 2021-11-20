d6c7eb861e68cb244eafc08cbc1359cb72f20d5b
Michael Desanker <mdesanker@gmail.com>
2021-11-19 22:19:21 -0500

    feat: fetch product category data from API

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,49 +1,79 @@
 import React, { Fragment, useEffect, useState } from "react";
 import styled from "styled-components";
 import ItemCard from "./ItemCard";
+import uniqid from "uniqid";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  const fetchData = async () => {
+  const fetchAPI = async () => {
     try {
-      const response = await fetch(`https://fakestoreapi.com/products`);
+      const [productResponse, categoryResponse] = await Promise.all([
+        fetch(`https://fakestoreapi.com/products`),
+        fetch(`https://fakestoreapi.com/products/categories`),
+      ]);
 
       // Error in fetch
-      if (!response.ok) {
-        const msg = `An error has occured: ${response.status}`;
+      if (!productResponse.ok) {
+        const msg = `An error has occured: ${productResponse.status}`;
+        throw new Error(msg);
+      }
+      if (!categoryResponse.ok) {
+        const msg = `An error has occured: ${categoryResponse.status}`;
         throw new Error(msg);
       }
 
-      const data = await response.json();
+      const products = await productResponse.json();
+      const categories = await categoryResponse.json();
 
-      setProducts(data);
+      setProducts(products);
+      setCategories(categories);
 
-      console.log(data);
+      console.log(products);
+      console.log(categories);
     } catch (error) {
       console.log(error.message);
     }
   };
 
   useEffect(() => {
-    fetchData();
+    fetchAPI();
   }, []);
 
   return (
-    <Fragment>
+    <Main>
       <h1>Products</h1>
+      <SortMenu>
+        <h2>Categories</h2>
+        <ul>
+          {categories.map((category) => {
+            return <li key={uniqid()}>{category}</li>;
+          })}
+        </ul>
+      </SortMenu>
       <ProductContainer>
         {products.map((item) => {
           return <ItemCard key={item.id} info={item} />;
         })}
       </ProductContainer>
-    </Fragment>
+    </Main>
   );
 };
+
+const SortMenu = styled.div`
+  width: 20%;
+  background-color: gray;
+`;
 
 const ProductContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
 `;
 
 export default Products;
