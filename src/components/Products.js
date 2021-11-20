7a75f2406251d8226a1e50dacb4cc7ad23553222
Michael Desanker <mdesanker@gmail.com>
2021-11-20 06:37:55 -0500

    feat: improve styling for product page

--- src/components/Products.js
+++ src/components/Products.js
@@ -59,48 +59,78 @@ const Products = () => {
 
   let menu = categories.map((category) => {
     return (
-      <li id={category} key={uniqid()} onClick={filterHandler}>
+      <Button id={category} key={uniqid()} onClick={filterHandler}>
         {category}
-      </li>
+      </Button>
     );
   });
 
   return (
     <Main>
-      <h1>Products</h1>
-      <SortMenu>
-        <h2>Categories</h2>
-        <ul>
-          <li id="all" onClick={filterHandler}>
+      <DisplayContainer>
+        <MenuContainer>
+          <p>Categories</p>
+          <Button id="all" onClick={filterHandler}>
             all
-          </li>
+          </Button>
           {menu}
-        </ul>
-      </SortMenu>
-      <ProductContainer>
-        {productDisplay.map((item) => {
-          return <ItemCard key={item.id} info={item} />;
-        })}
-      </ProductContainer>
+        </MenuContainer>
+        <CardContainer>
+          {productDisplay.map((item) => {
+            return <ItemCard key={item.id} info={item} />;
+          })}
+        </CardContainer>
+      </DisplayContainer>
     </Main>
   );
 };
 
 // styled components
-const SortMenu = styled.div`
-  width: 20%;
-  background-color: gray;
-`;
-
-const ProductContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 80%;
-`;
-
 const Main = styled.main`
   display: flex;
   flex-direction: column;
+  max-width: 1600px;
+  margin: 0 auto 50px;
+`;
+
+const MenuContainer = styled.div`
+  // position: fixed;
+  // top: 50px;
+  width: 20%;
+  max-width: 180px;
+  border-right: 1px solid gray;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+
+  p {
+    font-weight: 700;
+    margin-bottom: 20px;
+  }
+`;
+
+const Button = styled.button`
+  font-size: 1rem;
+  text-align: left;
+  padding: 3px;
+  border: none;
+  background: transparent;
+  transition: 100ms all;
+
+  &:hover {
+    color: blue;
+  }
+`;
+
+const DisplayContainer = styled.div`
+  display: flex;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 80%;
+  justify-content: space-evenly;
 `;
 
 export default Products;
